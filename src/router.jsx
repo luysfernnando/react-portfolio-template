db3bcb8906c0f80a3d8824828a3f7b6bc6798d95
Luys Fernnando <luysfernnando@gmail.com>
2023-03-29 15:55:17 -0300

    Update React Router

---
+++ src/router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+  <Route>
+    <Route path="/" />
+    <Route path="/about" />
+    <Route path="/portfolio" />
+    <Route path="/blog" />
+  </Route>
+);
